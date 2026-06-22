f8c8365f52de2fdc3806f36e3b7908ade6578dbd
DefNotVi <vice.escobar@duocuc.cl>
2026-06-22 16:50:22 -0400

    first commit

---
+++ src/main/java/cl/resumen/filler/FillerApplication.java
@@ -0,0 +1,13 @@
+package cl.resumen.filler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FillerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FillerApplication.class, args);
+	}
+
+}
